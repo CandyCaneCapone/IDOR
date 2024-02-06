bff4b4039f1081a8135b4202a7d3a2e1598fc4a1
amir hossein <amirfarvahar709@gmail.com>
2024-02-06 16:26:54 +0330

    add-signup-frontend

--- client/src/ui/Signup.jsx
+++ client/src/ui/Signup.jsx
@@ -1,8 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const navigate = useNavigate();
+
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/v1/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      if (response.status === 201) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.message === "BAD_REQUEST_ERROR") {
+        setError("Please provide valid data");
+      } else if (error.message === "UNAUTHENTICATED_ERROR") {
+        setError("Email already in use");
+      } else {
+        setError("An error occurred. Please try again later.");
+      }
+    }
+  };
+
+  useEffect(() => {});
+
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="w-full max-w-[40rem] mx-auto px-4 py-16 sm:px-6 lg:px-8">
@@ -11,13 +62,14 @@ function Signup() {
             Get started today
           </h1>
           <form
-            action=""
+            onSubmit={handleSubmit}
             className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
           >
             <p className="text-center text-lg font-medium">
               Sign up to your account
             </p>
 
+            {error && <p className="pl-4 pt-4 text-red-600">{error}</p>}
             <div>
               <label for="username" className="sr-only">
                 Username
@@ -25,6 +77,10 @@ function Signup() {
 
               <div className="relative">
                 <input
+                  name="username"
+                  required
+                  minLength={3}
+                  onChange={handleChange}
                   type="username"
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter username"
@@ -55,6 +111,9 @@ function Signup() {
 
               <div className="relative">
                 <input
+                  name="email"
+                  required
+                  onChange={handleChange}
                   type="email"
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter email"
@@ -86,6 +145,11 @@ function Signup() {
 
               <div className="relative">
                 <input
+                  name="password"
+                  required
+                  minLength={6}
+                  maxLength={30}
+                  onChange={handleChange}
                   type="password"
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter password"
@@ -116,7 +180,10 @@ function Signup() {
               </div>
             </div>
 
-            <button className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white">
+            <button
+              type="submit"
+              className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+            >
               Sign Up
             </button>
 
