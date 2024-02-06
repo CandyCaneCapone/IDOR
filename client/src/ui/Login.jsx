cc1684ff99a9ee71373d5495ea1544f88ec750b7
amir hossein <amirfarvahar709@gmail.com>
2024-02-06 17:08:36 +0330

    add-cookie-for-auth

--- client/src/ui/Login.jsx
+++ client/src/ui/Login.jsx
@@ -21,13 +21,17 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/v1/auth/signin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "http://localhost:5000/api/v1/auth/signin",
+        {
+          credentials : "include" , 
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
       if (!response.ok) {
         const errorData = await response.json();
         console.log(errorData);
@@ -39,7 +43,6 @@ function Login() {
       console.log(responseData);
 
       if (response.status === 200) {
-
         navigate("/");
       }
     } catch (error) {
